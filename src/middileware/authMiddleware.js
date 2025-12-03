0b11610d2dcfd8b3e71cf49fb82a4aad3cec7073
Kishore174 <skishore174@gmail.com>
2025-12-03 12:21:33 +0530

    branchesv03

--- src/middileware/authMiddleware.js
+++ src/middileware/authMiddleware.js
@@ -1,21 +1,27 @@
-// src/middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (roles = []) => {
   return (req, res, next) => {
-    const token = req.headers['authorization']?.split(' ')[1];
-    if (!token) {
-      return res.status(401).json({ success: false, message: 'Unauthorized' });
-    }
+
+    const authHeader =
+      req.headers.authorization || req.headers.Authorization;
+
+    if (!authHeader)
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+
+    const token = authHeader.split(" ")[1];
+    if (!token)
+      return res.status(401).json({ success: false, message: "Unauthorized" });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
-        return res.status(403).json({ success: false, message: 'Forbidden' });
+        return res.status(403).json({ success: false, message: "Forbidden" });
       }
 
-      req.user = decoded; 
+      req.user = decoded;
+
       if (roles.length && !roles.includes(decoded.role)) {
-        return res.status(403).json({ success: false, message: 'Forbidden' });
+        return res.status(403).json({ success: false, message: "Forbidden" });
       }
 
       next();
